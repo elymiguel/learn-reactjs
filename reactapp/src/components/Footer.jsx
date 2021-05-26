114dc1da2e75c4ff262698d2ef9adb18564d3206
elymiguel <user@MacBook-Pro.local>
2021-05-26 20:22:45 +0100

    Adicionei Image (Logo GitHub & Dio) Header & Footer

--- reactapp/src/components/Footer.jsx
+++ reactapp/src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+/** Image **/
+import srcLogoDio from '../img/diologo.png';
 
 const Footer = styled.footer`
 background-color: #eee;
@@ -13,17 +15,23 @@ width: 100%;
 height: 70px;
 `
 
-const FooterItem = styled.span`
+const FooterItem = styled.a`
 font-size: 18px;
 text-decoration: none;
 color: #777777;
+font-weight: bold;
 `
+
+const DioLogo = styled.img`
+    width: 150px;
+    height: 50px;
+`;
 
 export default function footer() {
   return(
       <React.Fragment>
         <Footer>
-            <FooterItem>Footer Item</FooterItem>
+            <FooterItem href="https://digitalinnovation.one" target="_blank"><DioLogo src={srcLogoDio}/></FooterItem>
         </Footer>
       </React.Fragment>
       
--- reactapp/src/components/Header.jsx
+++ reactapp/src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+/** Image **/
+import srcLogoGit from '../img/githublogo.png';
 
 const Header = styled.div`
 background-color: #eee;
@@ -14,17 +16,22 @@ height: 70px;
 
 const Items = styled.a`
 font-size: 18px;
-color: green;
+color: #777777;
 font-family; 'Read';
 font-weight: bold;
 padding: 0px 20px 0px 20px;
 `
 
+const GitLogo = styled.img`
+    width: 40px;
+    height: 40px;
+`;
+
 export default function header() {
     return (
     <Header>
       
-      <Items>Home</Items>
+      <Items href="https://github.com/elymiguel/learn-reactjs" target="_blank"><GitLogo src={srcLogoGit}/></Items>
       <Items>Contact</Items>
       <Items>About</Items>
       
